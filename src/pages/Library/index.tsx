c5ecc9bb94ac37563c62a4e802315cefddb93703
Nino Mercado <njesusmercado@gmail.com>
2024-08-01 23:37:14 -0500

    feat(pagination): finished pagination behavior

--- src/pages/Library/index.tsx
+++ src/pages/Library/index.tsx
@@ -1,27 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Stack, Pagination } from "@mui/material";
 import { BOOKS } from "../../constants";
 import { Searcher, Books } from "../../components/molecules";
+import { IBook } from "../../interfaces";
 
 export default function Library() {
 
+  const BOOKS_LIMIT = 10
   const [books, setBooks] = useState(BOOKS)
+  const [paginatedBooks, setPaginatedBooks] = useState<Array<IBook>>([])
   const [page, setPage] = useState<number>(1)
 
-  function handleOnChangePagination(event: React.ChangeEvent<unknown>, value: number) {
-    setPage(value);
+  function sliceBooks() {
+    const lowerLimit = (page-1)*BOOKS_LIMIT
+    var upperLimit = lowerLimit + BOOKS_LIMIT
+
+    if(upperLimit > books.length) upperLimit = books.length
+
+    return books.slice(lowerLimit, upperLimit)
   }
+
+  useEffect(() => setPaginatedBooks(sliceBooks()), [page, books])
+  useEffect(() => { if(page !== 1) setPage(1) }, [books])
 
   return (
     <main>
       <Stack direction='column' width='100' p={5} alignItems='center'>
         <Searcher data={BOOKS} onChange={setBooks}/>
-        <Books books={books}/>
+        <Books books={paginatedBooks}/>
         <Pagination
           sx={{ mt: 5 }}
-          count={10}
+          count={Math.ceil(books.length/BOOKS_LIMIT)}
           page={page}
-          onChange={handleOnChangePagination}
+          onChange={(event: React.ChangeEvent<unknown>, value: number) => setPage(value)}
           size='large'
         />
       </Stack>
