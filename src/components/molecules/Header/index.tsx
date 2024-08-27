58683c4946ae8a592e94800a331d06f86f4f235c
Nino Mercado <njesusmercado@gmail.com>
2024-08-27 16:52:47 -0500

    feat(Footer, Header): added styles to footer and header

--- src/components/molecules/Header/index.tsx
+++ src/components/molecules/Header/index.tsx
@@ -1,28 +1,33 @@
-import { Stack, styled } from "@mui/material";
+import { Stack, styled, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const StyledNav = styled('nav')`
-  padding: 5px 10px;
+  padding: 20px 10px;
+  background-color: var(--dark-brown);
 `
 
 export default function Header() {
   return (
     <StyledNav>
-      <Stack direction={"column"}>
-        <Stack direction={"row"} justifyContent={"space-between"}>
-          <Stack>LOGO</Stack>
-          <Stack direction={"row"}>
-            <Stack>Siguenos - Facebook - Instagram</Stack>
-            <Stack>Mi Cuenta</Stack>
+      <Stack direction="row" justifyContent='space-between'>
+        <img src="/images/logo.png" alt="" width='10%'/>
+        <Stack alignItems='flex-end' gap={2}>
+          <Stack direction={"row"} justifyContent={"space-between"}>
+            <Stack direction={"row"} color='var(--white)'>
+              <Stack>Siguenos - Facebook - Instagram</Stack>
+              <Stack>Mi Cuenta</Stack>
+            </Stack>
           </Stack>
-        </Stack>
-        <Stack direction={"row"} gap={2} justifyContent={"flex-end"}>
-          <Link to="/">Komensá</Link>
-          <Link to="/kajangari">Kajangarí</Link>
-          <Link to="/yurumbi">Yurumbí</Link>
-          <Link to="/arrelike">Arrelike</Link>
-          <Link to="/chakero">Chakero</Link>
-          <Link to="/suto">Suto</Link>
+          <Stack direction={"row"} gap={2} justifyContent={"flex-end"}>
+            <Link to="/">
+              <Typography color='var(--white)'>Komensá</Typography>
+            </Link>
+            <Link to="/kajangari">Kajangarí</Link>
+            <Link to="/yurumbi">Yurumbí</Link>
+            <Link to="/arrelike">Arrelike</Link>
+            <Link to="/chakero">Chakero</Link>
+            <Link to="/suto">Suto</Link>
+          </Stack>
         </Stack>
       </Stack>
     </StyledNav>
