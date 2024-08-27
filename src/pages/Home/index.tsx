5ee81cfc3d78f03b34a05d07d1b15da882cf11c1
Nino Mercado <njesusmercado@gmail.com>
2024-08-27 16:54:07 -0500

    feat(Home): styled home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,91 +1,86 @@
 'use client';
 
 import './index.css'
-import { Slider } from '../../components/molecules'
-import { Wave } from '../../components/atoms'
 import { Stack, Typography, Grid } from '@mui/material'
-import { Link } from 'react-router-dom'
-
-const styles = {
-  slide: {
-    padding: 15,
-    minHeight: 100,
-    color: '#fff',
-  },
-  slide1: {
-    background: '#FEA900',
-  },
-  slide2: {
-    background: '#B3DC4A',
-  },
-  slide3: {
-    background: '#6AC0FF',
-  },
-};
+import Wave from 'react-wavify';
+import { SectionButton } from '../../components/atoms';
+import { SECTIONS } from '../../constants/sections.constant';
+import { ISection } from '../../interfaces';
 
 export default function Home() {
-
-  function buildItems() {
-    return [
-      <div style={Object.assign({}, styles.slide, styles.slide1)}>
-        slide n°1
-      </div>,
-      <div style={Object.assign({}, styles.slide, styles.slide2)}>
-        slide n°2
-      </div>,
-      <div style={Object.assign({}, styles.slide, styles.slide3)}>
-        slide n°3
-      </div>
-    ]
-  }
-
   return (
     <main>
       <article>
         <section>
-          <Slider items={buildItems()} />
         </section>
-        <Wave>
-          <Stack textAlign={'center'} direction={'column'} justifyContent={'center'}>
-            <Typography variant={'h1'} fontWeight={'bold'}>Palenque</Typography>
-            <Typography variant='h3'>A un click</Typography>
-            <Stack
-              direction='row'
-              textAlign='center'
-              justifyContent='center'
-              mt={30}
-            >
-              <Typography
-                variant={'h5'}
-                sx={{
-                  wordWrap: 'break-word',
-                  maxWidth: '50ch'
-                }}
-              >
-                Kribí como herramienta digital es una solución interactiva y divertida; permite llegar a diferentes lugares que cuenten con acceso a internet, evitando que los usuarios tengan que trasladarse. También reduce valores negativos ambientales que se dan para acceder a escenarios de aprendizaje tradicional.
-              </Typography>
+        <section style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center' }}>
+          <Stack bgcolor='var(--yellow)' paddingTop={20}>
+            <Stack direction='row' justifyContent='center' gap={5}>
+              <Stack direction={'column'} height={'50vh'} justifyContent={'center'}>
+                <Typography variant={'h1'} fontWeight={'bold'}>Palenque</Typography>
+                <Typography variant='h3'>A un click</Typography>
+              </Stack>
+              <img
+                src="/images/1.png"
+                alt="Nino palenque sonriente mirando por la ventana de su casa"
+                style={{ borderRadius: '10px' }}
+                width='50%'
+              />
             </Stack>
-            <Stack direction='column' mt={10} sx={{ backgroundColor: 'red' }}>
-              <Typography variant='h3' fontWeight='bold'>
-                ¡CONOCE, APRENDE Y DIVIÉRTETE!
-              </Typography>
-              <Grid container direction='row' columns={2} columnSpacing={2}>
-                <Grid item xs={1} sx={{width: '100%'}}>
-                  <Link to='/kajangari'>Diccionario</Link>
-                </Grid>
-                <Grid item xs={1} sx={{width: '100%'}}>
-                  <Link to='/noticias'>Noticias</Link>
-                </Grid>
-                <Grid item xs={1} sx={{width: '100%'}}>
-                  <Link to='/actividades'>Actividades</Link>
-                </Grid>
-                <Grid item xs={1} sx={{width: '100%'}}>
-                  <Link to='/tienda'>Tienda</Link>
-                </Grid>
-              </Grid>
-            </Stack>
+            <Wave
+              style={{ display: 'flex', marginTop: '10vh' }}
+              fill='#fff6de'
+              paused={false}
+              options={{
+                amplitude: 40,
+                speed: 0.2,
+                points: 3
+              }}
+            />
           </Stack>
-        </Wave>
+          <Stack paddingTop={10}>
+            <Stack direction='row' justifyContent='center' alignItems='center' gap={5}>
+              <img
+                src="/images/2.png"
+                alt="Nino palenque serio mirando por la puerta de su casa"
+                style={{ borderRadius: '10px' }}
+                width='50%'
+              />
+              <Typography variant='h5' fontWeight='500' sx={{ wordWrap: 'break-word', maxWidth: '40ch', textAlign: 'left' }}>
+                Kribí como herramienta digital es una solución interactiva y divertida;
+                permite llegar a diferentes lugares que cuenten con acceso a internet,
+                evitando que los usuarios tengan que trasladarse. También reduce valores
+                negativos ambientales que se dan para acceder a escenarios de aprendizaje tradicional.
+              </Typography>
+            </Stack>
+            <Wave
+              style={{ display: 'flex', marginTop: '10vh' }}
+              fill='rgb(83, 34, 12)'
+              paused={false}
+              options={{
+                amplitude: 40,
+                speed: 0.2,
+                points: 3
+              }}
+            />
+          </Stack>
+          <Stack direction='row' sx={{ backgroundColor: 'var(--brown)' }} alignItems='center'>
+            <Typography variant='h3' fontWeight='bold' color='var(--white)'>
+              ¡CONOCE, APRENDE Y DIVIÉRTETE!
+            </Typography>
+            <img
+              src="/images/3.png"
+              alt="Nino palenque serio mirando por la puerta de su casa"
+              style={{ borderRadius: '10px' }}
+              width='50%'
+            />
+          </Stack>
+          <Stack bgcolor='var(--brown)' pb={10} pt={20}>
+            <Grid container direction='row' flexWrap='wrap' justifyContent='space-around' columnSpacing={2}>
+              { SECTIONS.map((section: ISection, index: number) => <SectionButton {...section} key={index} />) }
+            </Grid>
+          </Stack>
+        </section>
       </article>
     </main>
   )
