b680d06a5eb7afe1b4a6159bfe6a3e01c449af34
Nino Mercado <njesusmercado@gmail.com>
2024-09-16 14:11:52 -0500

    fix(SutoSection): fixed separation between translations

--- src/components/molecules/SutoSection/index.tsx
+++ src/components/molecules/SutoSection/index.tsx
@@ -1,6 +1,4 @@
 import { Stack, Typography } from "@mui/material";
-import { Speak } from "../../atoms";
-import { useState } from "react";
 
 interface SutoSectionProps {
   title: string;
@@ -10,18 +8,10 @@ interface SutoSectionProps {
 
 export default function SutoSection({ title, description, key }: SutoSectionProps) {
 
-  const [stop, setStop] = useState(true);
-
   return (
     <section style={{ marginTop: '100px' }} className="container-suto" key={key}>
       <Stack direction='row'>
         <Typography variant='h4'>{title}</Typography>
-        {/* TODO: fix speak error */}
-        <Speak
-          onClick={() => setStop(!stop)}
-          value={`${title} ${description}`}
-          stop={stop}
-        />
       </Stack>
       <Typography variant='body1' sx={{ mt: 5 }}>{description}</Typography>
     </section>
