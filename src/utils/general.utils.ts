205cedfdd8deb6241d725ec7d62b5505ae21b4a5
Nino Mercado <njesusmercado@gmail.com>
2025-02-13 02:24:27 -0500

    feat(screens.ts): added logic to differenciate if the host is in mobile

--- src/utils/general.utils.ts
+++ src/utils/general.utils.ts
@@ -1,9 +1,15 @@
 import { SocialMedias } from "../enums"
+import { SCREENS } from "../constants"
 
 function goTo(link: string) { window.open(link, '_blank') }
 function goToFacebook() { goTo(SocialMedias.FACEBOOK) }
 function goToInstagram () { goTo(SocialMedias.INSTAGRAM) }
 function goToGithub () { goTo(SocialMedias.GITHUB) }
+export function getWidth (): number { return window.screen.width }
+export function isSmallScreen (): boolean {
+  const screenWidth = getWidth()
+  return screenWidth < SCREENS.MD
+}
 
 export {
   goTo,
