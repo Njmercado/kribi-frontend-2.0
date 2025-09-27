bb791f01843b0d0b9414a60c28a5e3c953ff7c81
Nino Mercado <njesusmercado@gmail.com>
2025-09-27 18:37:43 -0500

    feature(library): improved library design and logic. Deleted unnecesary component

--- src/components/molecules/FilterSearcher/index.tsx
+++ src/components/molecules/FilterSearcher/index.tsx
@@ -1,69 +1,68 @@
 import './index.css';
 import { useEffect, useState } from 'react';
-import { BaseSearcher, MultiSelect } from '../../atoms';
+import { BaseSearcher } from '../../atoms';
+import { Chip, Grid2, Stack } from '@mui/material';
 
-export interface FilterSearcherProps<T extends object, K extends keyof T> {
-  items: Array<T>
-  onChange: (value: Array<T>) => void
+export interface FilterSearcherProps {
+  onChange: (query: string, filter: Array<string> | undefined) => void
   bgColor?: string
   color?: string
   placeholder: string
-  options: Array<string> | { [key: string]: string } | any
-  propertyToFilterBy: K
-  filterByOptions: (value: Array<T>, chosenOptions: Array<string>) => Array<T>
+  filterOptions?: Array<string> | { [key: string]: string }
 }
 
-export default function FilterSearcher<T extends object, K extends keyof T>({
-  items,
+export default function FilterSearcher({
   onChange,
   bgColor = 'var(--white)',
   color = 'var(--brown)',
   placeholder,
-  options,
-  propertyToFilterBy,
-  filterByOptions
-}: FilterSearcherProps<T, K>) {
+  filterOptions,
+}: FilterSearcherProps) {
   const [searchQuery, setSearchQuery] = useState('');
-  const [chosenOptions, setChosenOptions] = useState<Array<string>>([]);
-  const [initialItems, setInitialItems] = useState<Array<T>>([])
-
-  useEffect(() => { setInitialItems(items) }, [])
+  const [chosenOptions, setChosenOptions] = useState<{ [key: string]: boolean }>({});
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const query = e.target.value;
     setSearchQuery(query);
-    filterDocuments(query, chosenOptions);
   };
 
-  const handleOnMultiSelectChange = (options: Array<string>) => {
-    setChosenOptions(options)
-    filterDocuments(searchQuery, options);
-  };
+  useEffect(() => {
+    const selectedFilters = Object
+      .keys(chosenOptions)
+      .filter((key) => chosenOptions[key])
+      .map((key) => (filterOptions as { [key: string]: string })[key]);
 
-  const filterDocuments = (query: string, chosenOptionsProp: Array<string>) => {
-    const filteredDocumentsByValue: Array<T> = filterByValue(query)
-    const filteredDocumentsByOpions = filterByOptions(filteredDocumentsByValue, chosenOptionsProp)
-    onChange(filteredDocumentsByOpions);
-  };
+    onChange(searchQuery, selectedFilters);
+  }, [searchQuery, chosenOptions]);
 
-  const filterByValue = (query: string): Array<T> => {
-
-    if (query.length === 0) return initialItems;
-
-    return initialItems.filter((item: T) => {
-      const valueToCompare = (item[propertyToFilterBy] as string).toLowerCase();
-      const queryLower = query.toLowerCase();
-      return valueToCompare.includes(queryLower);
-    });
+  function handleChosenFilter(keyOption: string) {
+    if (!chosenOptions[keyOption]) {
+      setChosenOptions((options) => ({ ...options, [keyOption]: true }))
+    } else {
+      setChosenOptions((options) => ({ ...options, [keyOption]: !chosenOptions[keyOption] }))
+    }
   }
 
   return (
-    <BaseSearcher
-      placeholder={placeholder}
-      onChange={handleSearch}
-      action={<MultiSelect color={color} options={options} onChange={handleOnMultiSelectChange} />}
-      bgColor={bgColor}
-      color={color}
-    />
+    <Stack direction='column' justifyContent='center' alignItems='center' width='100%' p={1} spacing={2} >
+      <BaseSearcher
+        placeholder={placeholder}
+        onChange={handleSearch}
+        bgColor={bgColor}
+        color={color}
+      />
+      <Grid2 container flex='wrap' direction='row' spacing={1} justifyContent='center' alignItems='center'>
+        {
+          filterOptions && Object.keys(filterOptions || {}).map((key: string) => (
+            <Chip
+              key={key}
+              label={(filterOptions as { [key: string]: string } )[key]}
+              sx={{ backgroundColor: chosenOptions[key] ? 'var(--dark-yellow)' : 'var(--yellow)' }}
+              onClick={() => handleChosenFilter(key)}
+            />
+          ))
+        }
+      </Grid2>
+    </Stack>
   )
-};
+}
